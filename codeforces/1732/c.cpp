a888ad23be8552c8368979ea973e1115828f3ccb
Mohit Kumar Agrawal <mhtkrag@gmail.com>
2022-10-26 06:54:26 +0530

    solved c1 and c2 and d1

--- codeforces/1732/c.cpp
+++ codeforces/1732/c.cpp
@@ -4,38 +4,71 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 
+ll arr[100'005];
+ll px[100'005], ps[100'005];
+ll jmp[100'005];
+
 void solve() {
   ll n, _;
   cin >> n >> _;
 
-  vector<ll> arr(n + 1);
   for (ll i = 1; i <= n; ++i) cin >> arr[i];
 
-  vector<ll> ps(n + 1), px(n + 1);
+  jmp[n+1] = n+1;
+  for (ll i = n; i >= 0; --i) {
+    if (arr[i] == 0) {
+      jmp[i] = jmp[i+1];
+    } else {
+      jmp[i] = i;
+    }
+  }
+
   ps[0] = 0;
   px[0] = 0;
   for (ll i = 1; i <= n; ++i) ps[i] = ps[i - 1] + arr[i];
   for (ll i = 1; i <= n; ++i) px[i] = px[i - 1] ^ arr[i];
 
+  auto calc_val = [&](ll l, ll r) -> ll {
+    return (ps[r] - ps[l - 1]) - (px[r] ^ px[l - 1]);
+  };
+
   ll left, right;
-  cin >> left >> right;
+  while (_--) {
+    cin >> left >> right;
 
-  ll val = (ps[right] - ps[left - 1]) - (px[right] ^ px[left - 1]);
+    ll val = calc_val(left, right);
 
-  while (right > left) {
-    ll l_val = (ps[right] - ps[left]) - (px[right] ^ px[left]);
-    ll r_val = (ps[right - 1] - ps[left - 1]) - (px[right - 1] ^ px[left - 1]);
+    int left_removal_count = 0;
+    ll ans_l = jmp[left] > right ? right : jmp[left], ans_r = right;
 
-    if (l_val < val && r_val < val) break;
+    for (ll l = jmp[left]; l <= right; l = jmp[l + 1]) {
+      if (left_removal_count >= 32) break;
+      if (calc_val(l, right) < val) break;
 
-    if (l_val <= r_val) {
-      right--;
-    } else {
-      left++;
+      if (arr[l] != 0) ++left_removal_count;
+
+      ll r = right;
+      ll low = l, high = right;
+      while (high >= low) {
+        ll mid = (low + high) / 2;
+        ll v = calc_val(l, mid);
+
+        if (v == val) {
+          r = mid;
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      }
+
+      if ((r - l) < (ans_r - ans_l)) {
+        ans_l = l;
+        ans_r = r;
+      }
     }
-  }
 
-  cout << left << " " << right << endl;
+    cout << ans_l << " " << ans_r << endl;
+  }
 }
 
 int main() {
@@ -43,5 +76,6 @@ int main() {
 
   ll t;
   cin >> t;
-  while (t--) solve();
+  while (t--)
+    solve();
 }
--- codeforces/1732/d.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#define all(x) begin(x), end(x)
-
-using namespace std;
-using ll = long long;
-
-int main() {
-
-}
